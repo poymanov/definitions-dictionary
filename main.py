cd3f37258b8379ad7a3687d0b0a65e88030aa12e
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-11 00:16:33 +0300

    Add suggest word func

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import json
-
-def find_word(data, word):
-	word = word.lower()	
-	
-	if word in data:
-		return data[word]
-	else:
-		return ["Word doesn't exists. Please check it double"]
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
-word = input('Type word that you want find in dictionary: ')
+messages = {
+	'type_messages': 'Type word that you want find in dictionary: ',
+	'word_not_exist': "Word doesn't exists. Please check it double",
+	'suggest_message': 'Do you mean %s instead? (Y - Yes, N - No): ',
+	'wrong_entry': 'Wrong entry value'
+}
 
-result = find_word(data, word)
+def find_word(word):
+	word = word.lower()	
+
+	if word in data:
+		return data[word]
+	else:
+		suggest_words = get_close_matches(word, data.keys())
+
+		if len(suggest_words) >= 1:
+			answer = input(messages['suggest_message'] % suggest_words[0])			
+
+			if answer == 'Y':
+				return data[suggest_words[0]]
+			elif answer == 'N':
+				return [messages['word_not_exist']]
+			else:
+				return [messages['wrong_entry']]	
+		else:
+			return [messages['word_not_exist']]
+
+word = input(messages['type_messages'])
+
+result = find_word(word)
 
 print('')
 
